7e64b0f8a5181e2c9db967272e2b64d607e65d4c
boicu_claudiu <boicu_petronel@yahoo.com>
2025-11-13 15:17:29 +0200

    fixes

--- src/app.tsx
+++ src/app.tsx
@@ -12,11 +12,9 @@ const getUrls = searchTerm => [
 	`https://www.redgoblin.ro/search?q=${searchTerm}`,
 	`https://www.barlogulcujocuri.ro/produse?c=${searchTerm}`,
 	`https://shop.guildhall.ro/produse?c=${searchTerm}`,
-	`https://www.jocozaur.ro/search?options%5Bprefix%5D=last&type=product%2Carticle%2Cpage&q=${searchTerm}`,
 	`https://www.pionul.ro/index.php?route=product/search&search=${searchTerm}&description=true`,
 	`https://www.lexshop.ro/produse?c=${searchTerm}`,
-	`https://www.regatuljocurilor.ro/ro/cautare?controller=search&orderby=position&orderway=desc&search_query=${searchTerm}&submit_search=`,
-	`https://www.dragongames.ro/produse?c=${searchTerm}`
+	`https://www.regatuljocurilor.ro/ro/cautare?controller=search&orderby=position&orderway=desc&search_query=${searchTerm}&submit_search=`
 ];
 
 function App() {
